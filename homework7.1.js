de07635a52b80e1998b0a0eedd4be17a4290e7e4
Alla Varavina <fistawka27@gmail.com>
2025-01-19 23:34:48 +0100

    fix

--- homework7.1.js
+++ homework7.1.js
@@ -16,8 +16,6 @@ var arr = [
   },
 ];
 const jsonString = JSON.stringify(arr);
-
 const trustedEmailRegex = /[\w.-]+@(gmail|yahoo)\.com/g;
-
 const trustedEmails = jsonString.match(trustedEmailRegex);
 console.log(trustedEmails);
