aa40123f44ab084c6a4a83761aa294c22e38de6e
Alla Varavina <fistawka27@gmail.com>
2025-01-19 23:33:17 +0100

    fix

--- homework7.1.js
+++ homework7.1.js
@@ -1,6 +1,23 @@
+var arr = [
+  {
+    userName: 'Test',
+    lastName: 'Test',
+    email: 'test.test@gmail.com',
+  },
+  {
+    userName: 'Dmitro',
+    lastName: 'Porohov',
+    email: 'dmitro.porohov@yahoo.com', // Синтаксична помилка, але email правильний
+  },
+  {
+    userName: 'Andrii',
+    lastName: '',
+    email: 'andrii@mail.ru', // Нам такі не підходять
+  },
+];
 const jsonString = JSON.stringify(arr);
 
-const trustedEmailRegex = /^[\w.-]+@(gmail\.com|yahoo\.com)$/;
+const trustedEmailRegex = /[\w.-]+@(gmail|yahoo)\.com/g;
 
 const trustedEmails = jsonString.match(trustedEmailRegex);
 console.log(trustedEmails);
